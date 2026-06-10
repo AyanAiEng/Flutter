ae0da8aa592ba8fa682d2f5de5764b9cbfec8e17
Ayan Ali <braincoder.ai@gmail.com>
2026-06-10 07:49:22 +0500

    Flutter

---
+++ Flutter_Scaffolding/lib/main.dart
@@ -0,0 +1,142 @@
+/* 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+          body: Center(
+            child: Text(
+              'Hello World',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.pink,
+               ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+     MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hey"),
+        ),
+        body: Center(
+          child: Text("Welcome"),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+    ),
+  );
+}
+
+ */
+/* 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+          body: Center(
+            child: Text(
+              'Hello World',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.pink,
+               ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+     MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hey"),
+        ),
+        body: Center(
+          child: Text("Welcome"),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+    ),
+  );
+}
+
+
+import 'package:flutter/material.dart';c
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("hey"),
+        ),
+        backgroundColor: Colors.yellow,
+        body: const Center(
+          child: Text(
+            "Welcome",
+            style: TextStyle(color: Colors.red),
+          ),
+        ),
+      ),
+    ),
+  );
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Hello"), centerTitle: true),
+        backgroundColor: Colors.green,
+        body: con Center(
+          child: Text(
+            "Welcome",
+            style: TextStyle(color: Colors.orange),
+            Image.network(
+              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg`,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+}
+
+ */
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Hello"), centerTitle: true),
+        backgroundColor: Colors.green,
+        body: Center(
+          child: Image.network(
+            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+          ),
+        ),
+      ),
+    ),
+  );
+}
